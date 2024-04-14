a57da4f110cff4078b4ae9c40c11e0712a4db63e
hussainalboori <hussainalboori@gmail.com>
2024-04-14 14:23:09 +0300

    new password now adding to the database

--- data/addnewpassword.go
+++ data/addnewpassword.go
@@ -1,30 +1,34 @@
 package data
 
 import (
-    "database/sql"
-    //"errors"
+	"database/sql"
+	"errors"
+	//"errors"
 )
 
 // InsertPassword inserts a new password into the database
 func InsertPassword(userID int, website, username, password string) error {
-    // SQL statement to insert a new password
-    insertPasswordSQL := `
+	if website == "" || username == "" || password == "" || userID == 0 {
+		return errors.New("missing required fields")
+	}
+	// SQL statement to insert a new password
+	insertPasswordSQL := `
         INSERT INTO passwords (user_id, website, username, password)
         VALUES (?, ?, ?, ?);
     `
 
-    // Open a connection to the SQLite3 database
-    db, err := sql.Open("sqlite3", "data.db")
-    if err != nil {
-        return err
-    }
-    defer db.Close()
+	// Open a connection to the SQLite3 database
+	db, err := sql.Open("sqlite3", "data.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
 
-    // Execute the SQL statement to insert the password
-    _, err = db.Exec(insertPasswordSQL, userID, website, username, password)
-    if err != nil {
-        return err
-    }
+	// Execute the SQL statement to insert the password
+	_, err = db.Exec(insertPasswordSQL, userID, website, username, password)
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
--- handler/new.go
+++ handler/new.go
@@ -4,73 +4,101 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"password-manger/data"
+	"strconv"
 	"text/template"
 )
 
 func HandleNewPassword(w http.ResponseWriter, r *http.Request) {
-	_, sessionData, exists := getSession(r)
-	if !exists {
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		return
-	}
-	data := struct {
-		UserID   string
-		Username string
-	}{
-		UserID:   sessionData["userID"],
-		Username: sessionData["username"],
-	}
+	var id int
+	if r.Method == "GET" {
+		_, sessionData, exists := getSession(r)
+		userIDstr := sessionData["userID"]
+		userID, err := strconv.Atoi(userIDstr)
+		if err != nil {
+			log.Printf("Error converting userID to int: %v\n", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		if !exists {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		data := struct {
+			UserID   int
+			Username string
+		}{
+			UserID:   userID,
+			Username: sessionData["username"],
+		}
 
-	// Load the template file
-	tmpl, err := template.ParseFiles("templates/new.html")
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		fmt.Printf("Error loading template file: %v\n", err)
-		return
-	}
-
-	errsen := tmpl.Execute(w, data)
-	if errsen != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		fmt.Printf("Error executing template: %v\n", err)
-	}
-
-	// Check if the request method is POST
-	if r.Method == http.MethodPost {
-		// Get the form values
-		userID := r.PostForm.Get("user_id")
-		website := r.PostForm.Get("website")
-		username := r.PostForm.Get("username")
-		password := r.PostForm.Get("password")
-		// userIDInt, err := strconv.Atoi(userID)
+		// Load the template file
+		tmpl, err := template.ParseFiles("templates/new.html")
 		if err != nil {
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			fmt.Printf("Error converting userID to integer: %v\n", err)
+			fmt.Printf("Error loading template file: %v\n", err)
 			return
 		}
 
-		log.Printf("user: %v\n", userID)
-		log.Printf("website: %v\n", website)
-		log.Printf("username:%v\n", username)
-		log.Printf("password: %v\n", password)
-
-		// Insert the new password into the database
-		// err = data.InsertPassword(userIDInt, website, username, password)
-		// if err != nil {
-		// 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		// 	fmt.Printf("Error inserting password into database: %v\n", err)
-		// 	return
-		// }
-
-		// Redirect the user to a success page
-		http.Redirect(w, r, "/success", http.StatusSeeOther)
-		return
+		errsen := tmpl.Execute(w, data)
+		if errsen != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			fmt.Printf("Error executing template: %v\n", err)
+			return
+		}
+		id = userID
+		fmt.Printf("User ID 49 : %v\n", id)
 	}
 
-	// Execute the template with empty data
-	err = tmpl.Execute(w, nil)
+	// if r.Method != http.MethodPost {
+	// 	http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	// 	log.Printf("Method Not Allowed:gg %v\n", r.Method)
+	// 	return
+	// }
+	fmt.Printf("User ID 56 : %v\n", id)
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		log.Printf("Error parsing form: %v\n", err)
+	}
+	if id == 0 {
+		_, sessionData, exists := getSession(r)
+		userIDstr := sessionData["userID"]
+		userID, err := strconv.Atoi(userIDstr)
+		if err != nil {
+			log.Printf("Error converting userID to int: %v\n", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		if !exists {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		data := struct {
+			UserID   int
+		}{
+			UserID:   userID,
+		}
+
+		id = data.UserID
+	}
+
+	// Get the form values
+	website := r.PostForm.Get("website")
+	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+
+	// if website == "" || username == "" || password == "" || id == 0 {
+	// 	http.Error(w, "Bad Request", http.StatusBadRequest)
+	// 	return
+	// }
+	err = data.InsertPassword(id , website, username, password)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		fmt.Printf("Error executing template: %v\n", err)
+		log.Printf("Error inserting password: %v\n", err)
+		return
+	} else {
+		http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+		log.Printf("Password inserted successfully\n")
 	}
 }
